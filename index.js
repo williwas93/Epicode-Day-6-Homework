f05f1af00dffb9184149801b5be1ece2d37caaff
williwas93 <108356460+williwas93@users.noreply.github.com>
2022-07-04 16:20:24 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+// "This is my first JS query - Wish me luck!";
+let welcome = "Welcome to my first JS Query - wish me luck!";
+
+let firstname;
+
+firstname = "William ";
+
+let middlename;
+
+middlename = "James ";
+
+let surname;
+
+surname "Rowe ";
+
+console.log("\n------- Tell me what your name is --------");
+
+
