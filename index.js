c8d8749c8e23136813c1c38b922a5a1ebc2a2683
williwas93 <108356460+williwas93@users.noreply.github.com>
2022-07-04 16:47:29 +0100

    update

--- index.js
+++ index.js
@@ -11,8 +11,26 @@ middlename = "James ";
 
 let surname;
 
-surname "Rowe ";
+surname = "Rowe ";
+
+let age;
+
+age = "29";
+
+console.log (welcome);
 
 console.log("\n------- Tell me what your name is --------");
+console.log(firstname + surname);
 
+console.log ("\n------- Okay great, do you have a middle name? -------");
+console.log (middlename);
 
+console.log("\n---------- So what is your full name then? -----------");
+console.log (firstname + middlename + surname);
+
+console.log("\n----------- Why don't you introduce yourself please? ----------");
+console.log("\nHello my name is ");
+console.log(firstname + middlename + surname);
+
+console.log("\n----------- How old are you? ----------");
+console.log(age);
